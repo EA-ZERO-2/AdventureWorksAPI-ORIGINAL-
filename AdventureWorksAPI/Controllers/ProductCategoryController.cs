a25a16a966b28dca3beccb51f3a5561c4008ced9
Erick Haroldo Bola?os Alvarez <erick.alvarez@galileo.edu>
2022-09-02 13:17:02 -0600

    Add project files.

---
+++ AdventureWorksAPI/Controllers/ProductCategoryController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AdventureWorksNS.Data;
+using AdventureWorksAPI.Repositories;
+
+
+namespace AdventureWorksAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+        private readonly IProductCategoryRepository repo;
+
+        public ProductCategoryController(IProductCategoryRepository repository)
+        {
+            this.repo = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ProductCategory>))]
+        public async Task<IEnumerable<ProductCategory>> GetProductCategories(string? productName)
+        {
+            if (string.IsNullOrEmpty(productName))
+            {
+                return await repo.RetrieveAllAsync();
+            }
+            else
+            {
+                return (await repo.RetrieveAllAsync())
+                        .Where(productCategory => productCategory.Name == productName);
+            }
+        }
+
+        [HttpGet("{id}", Name  = nameof(GetProductCategories))] //Ruta
+        [ProducesResponseType(200, Type = typeof(ProductCategory))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetProductCategories(int id)
+        {
+            ProductCategory? p = await repo.RetrieveAsync(id);
+            if (p == null)
+            {
+                return NotFound(); //Error 404
+            }
+            return Ok(p);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(ProductCategory))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Create([FromBody] ProductCategory p)
+        {
+            if (p == null)
+            {
+                return BadRequest(); //400
+            }
+            ProductCategory? addProductoCategory = await repo.CreateAsync(p);
+            if (addProductoCategory == null)
+            {
+                return BadRequest("El repositorio fallo al crear el productCategory");
+            }
+            else
+            {
+                return CreatedAtRoute(
+                        routeName: nameof(GetProductCategories),
+                        routeValues: new { id = addProductoCategory.ProductCategoryId },
+                        value: addProductoCategory);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Update(int id, [FromBody] ProductCategory p)
+        {
+            if (p == null || p.ProductCategoryId != id)
+            {
+                return BadRequest(); //400
+            }
+            ProductCategory? existe = await repo.RetrieveAsync(id);
+            if (existe == null)
+            {
+                return NotFound(); //404
+            }
+            await repo.UpdateAsync(id, p);
+            return new NoContentResult(); //204
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            ProductCategory? existe = await repo.RetrieveAsync(id);
+            if (existe == null)
+            {
+                return NotFound(); //404
+            }
+            bool? deleted = await repo.DeleteAsync(id);
+            if (deleted.HasValue && deleted.Value)
+            {
+                return new NoContentResult(); //201
+            }
+            return BadRequest($"Product y la Categoria con el id {id} no se pudo borrar");
+        }
+
+    }
+}
---
+++ AdventureWorksAPI/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using AdventureWorksNS.Data;
+using System.Collections.Concurrent;
+
+namespace AdventureWorksAPI.Repositories
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private static ConcurrentDictionary<int, ProductCategory>? producCategoryCache;
+        //Comentario, puede usar Redis para un cache mas eficiente ==> Open Source
+        private AdventureWorksDB db;
+
+        public ProductCategoryRepository(AdventureWorksDB injectedDB)
+        {
+            db = injectedDB;
+            if (producCategoryCache is null)
+            {
+                producCategoryCache = new ConcurrentDictionary<int, ProductCategory>(
+                    db.ProductCategories.ToDictionary(p => p.ProductCategoryId));
+            }
+        }
+
+        public async Task<ProductCategory> CreateAsync(ProductCategory p)
+        {
+            EntityEntry<ProductCategory> agregado = await db.ProductCategories.AddAsync(p);
+            int afectados = await db.SaveChangesAsync();
+            if (afectados == 1)
+            {
+                if (producCategoryCache is null) return p;
+                return producCategoryCache.AddOrUpdate(p.ProductCategoryId, p, UpdateCache);
+            }
+            return null!;
+        }
+        private ProductCategory UpdateCache(int id, ProductCategory p)
+        {
+            ProductCategory? antiguo;
+            if (producCategoryCache is not null)
+            {
+                if (producCategoryCache.TryGetValue(id, out antiguo))
+                {
+                    if (producCategoryCache.TryUpdate(id, p, antiguo))
+                    {
+                        return p;
+                    }
+                }
+            }
+            return null!;
+        }
+        public Task<IEnumerable<ProductCategory>> RetrieveAllAsync()
+        {
+            return Task.FromResult(producCategoryCache is null ?
+                Enumerable.Empty<ProductCategory>() : producCategoryCache.Values);
+        }
+
+        public Task<ProductCategory?> RetrieveAsync(int id)
+        {
+            if (producCategoryCache is null) return null!;
+            producCategoryCache.TryGetValue(id, out ProductCategory? p);
+            return Task.FromResult(p);
+        }
+        public async Task<ProductCategory?> UpdateAsync(int id, ProductCategory p)
+        {
+            db.ProductCategories.Update(p);
+            int afectados = await db.SaveChangesAsync();
+            if (afectados == 1)
+            {
+                return UpdateCache(id, p);
+            }
+            return null;
+        }
+        public async Task<bool?> DeleteAsync(int id)
+        {
+            ProductCategory? p = db.ProductCategories.Find(id);
+            if (p is null) return false;
+            db.ProductCategories.Remove(p);
+            int afectados = await db.SaveChangesAsync();
+            if (afectados == 1)
+            {
+                if (producCategoryCache is null) return null;
+                return producCategoryCache.TryRemove(id, out p);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
